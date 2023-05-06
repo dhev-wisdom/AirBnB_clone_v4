49ca42e180a1ce3b7babcf08b92003df72e2d968
dhev-wisdom <nonsowisdom62@gmail.com>
2023-05-06 15:20:07 +0100

    task 1

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  let checkedBoxList = {};
+  $('input[type:checkbox]').on('change', function () {
+    // const amenityID = $(this).data('id');
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+    if ($(this).is(':checked')) {
+      checkedBoxList[amenityId] = amenityName;
+    } else {
+      delete checkedBoxList[amenityId];
+    }
+
+    const h4Tag = $('div > h4');
+  });
+});
